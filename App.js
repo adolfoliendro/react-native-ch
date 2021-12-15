c5fd153dc8f9fb628a903da7f1a837ccd9732e94
Benja Liendro <benja.liendro@gmail.com>
2021-12-14 23:22:33 -0300

    Entrega encuentro 2 - Lista optimizada

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [list, setList] = useState([]);
+  const [textInput, setTextInput] = useState("");
+
+  const onAdd = () => {
+    setList([
+      ...list,
+      { id: Math.floor(Math.random() * 100) + 1, value: textInput },
+    ]);
+  };
+
+  const onDelete = (id) => {
+    setList(list.filter((item) => item.id != id));
+  };
+
   return (
     // <View style={styles.container}>
     //   <Text>Hola, Coder!</Text>
@@ -10,15 +31,35 @@ export default function App() {
     // </View>
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Items de lista" style={styles.input} />
-        <Button title="ADD" />
+        <TextInput
+          placeholder="Escribir item"
+          style={styles.input}
+          onChangeText={(text) => setTextInput(text)}
+        />
+        <Button onPress={() => onAdd()} title="ADD" />
       </View>
       <View style={styles.listContainer}>
-        <Text style={styles.listElement}>Elemento 1</Text>
-        <Text style={styles.listElement}>Elemento 2</Text>
-        <Text style={styles.listElement}>Elemento 3</Text>
-        <Text style={styles.listElement}>Elemento 4</Text>
-        <Text style={styles.listElement}>Elemento 5</Text>
+        {list.length > 0 ? (
+          // (
+          // list.map((item) => (
+          //   <View style={styles.listElement}>
+          //     <Text>{item}</Text>
+          //     <Button onPress={() => onDelete(item)} title="X" />
+          //   </View>
+          // )))
+          <FlatList
+            data={list}
+            renderItem={(data) => (
+              <View style={styles.listElement}>
+                <Text>{data.item.value}</Text>
+                <Button onPress={() => onDelete(data.item.id)} title="X" />
+              </View>
+            )}
+            keyExtractor={(item) => item.id}
+          />
+        ) : (
+          <Text> No hay tareas </Text>
+        )}
       </View>
     </View>
   );
@@ -42,13 +83,14 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     backgroundColor: "#ddd",
-    paddingHorizontal: 10,
-    paddingBottom: 10,
+    padding: 20,
     marginTop: 20,
   },
   listElement: {
     backgroundColor: "#aaf",
     padding: 10,
     marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
